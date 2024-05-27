1dce5c32ff5c4d13c82fa0e6ab283516ab72de20
Saint <sikim0721@gmail.com>
2024-05-27 12:51:52 +0900

    Homeview 수정

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -8,45 +8,10 @@ import VerticalBarChart from "../components/VerticalBarChart";
 import Header from "../components/Hearder";
 
 function HomeView() {
-  const [orgRows, setOrgRows] = useState([
-    {
-      id: 1,
-      date: "2022-12-02",
-      title: "점심",
-      category: "식비",
-      tag: "지출",
-      amount: 8000,
-    },
-    {
-      id: 2,
-      date: "2022-12-02",
-      title: "간식",
-      category: "식비",
-      tag: "지출",
-      amount: 2000,
-    },
-    {
-      id: 3,
-      date: "2022-12-02",
-      title: "영화",
-      category: "문화/교육비",
-      tag: "지출",
-      amount: 12000,
-    },
-  ]);
-
+  const [orgRows, setOrgRows] = useState([]);
   const [rows, setRows] = useState(orgRows);
   const [income, setIncome] = useState(0);
   const [expense, setExpense] = useState(0);
-  const [count, setCount] = useState(0);
-
-  const [dataList, setDataList] = useState({
-    food: 0,
-    goods: 0,
-    edu: 0,
-    etc: 0,
-    save: 0,
-  });
 
   useEffect(() => {
     setRows(orgRows);
@@ -67,45 +32,6 @@ function HomeView() {
     ++nextId.current;
   };
 
-  useEffect(() => {
-    setDataList({
-      food: 0,
-      goods: 0,
-      edu: 0,
-      etc: 0,
-      save: 0,
-    });
-
-    rows.forEach((item) => {
-      if (item.category === "식비") {
-        setDataList((prev) => ({
-          ...prev,
-          food: prev.food + item.amount,
-        }));
-      } else if (item.category === "생필품") {
-        setDataList((prev) => ({
-          ...prev,
-          goods: prev.goods + item.amount,
-        }));
-      } else if (item.category === "문화/교육비") {
-        setDataList((prev) => ({
-          ...prev,
-          edu: prev.edu + item.amount,
-        }));
-      } else if (item.category === "기타") {
-        setDataList((prev) => ({
-          ...prev,
-          etc: prev.etc + item.amount,
-        }));
-      } else if (item.category === "저축") {
-        setDataList((prev) => ({
-          ...prev,
-          save: prev.save + item.amount,
-        }));
-      }
-    });
-  }, [rows]);
-
   function totalIncomeHandler(data) {
     setIncome(data);
   }
@@ -124,21 +50,6 @@ function HomeView() {
     setRows(newRows);
   };
 
-  const changeTagHandler = () => {
-    setCount(count + 1);
-    console.log(count);
-    let newRows = [];
-    if (count % 3 === 0) {
-      newRows = orgRows.filter((item) => item.tag !== "수입");
-      setRows(newRows);
-    } else if (count % 3 === 1) {
-      newRows = orgRows.filter((prevState) => prevState.tag !== "지출");
-      setRows(newRows);
-    } else {
-      setRows(orgRows);
-    }
-  };
-
   return (
     <>
       <Header />
@@ -150,14 +61,12 @@ function HomeView() {
             totalIncome={totalIncomeHandler}
             totalExpense={totalExpenseHandler}
             monthFilter={changeMonthHandler}
-            tagFilter={changeTagHandler}
           />
           <VerticalBarChart orgRows={rows} />
         </AccountArea>
         <ChartArea
           totalExpense={expense}
           totalIncome={income}
-          dataList={dataList}
           orgRows={orgRows}
         />
       </AccountTempleat>
